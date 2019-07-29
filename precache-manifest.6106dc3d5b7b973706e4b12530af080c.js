6d40fda858aa09de65b3c15f69141a519d23c988
Tony Soukkeo <TonySoukkeo@Tonys-MacBook-Pro.local>
2019-07-29 15:50:55 -0600

    Updates

--- precache-manifest.6106dc3d5b7b973706e4b12530af080c.js
+++ precache-manifest.6106dc3d5b7b973706e4b12530af080c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ce56e728035e10a048f86b9f57ab992b",
+    "revision": "af7de028d86f4785d132404a46b02e43",
     "url": "/HSDrywall/index.html"
   },
   {
-    "revision": "9f044169ae8b7ea15eaf",
+    "revision": "ba13837d1b63bbf424cc",
     "url": "/HSDrywall/static/css/main.6c2138a9.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/HSDrywall/static/js/2.82358439.chunk.js"
   },
   {
-    "revision": "9f044169ae8b7ea15eaf",
-    "url": "/HSDrywall/static/js/main.d88955ee.chunk.js"
+    "revision": "ba13837d1b63bbf424cc",
+    "url": "/HSDrywall/static/js/main.c2eb9b71.chunk.js"
   },
   {
     "revision": "6e46dffe07a92a11e928",
